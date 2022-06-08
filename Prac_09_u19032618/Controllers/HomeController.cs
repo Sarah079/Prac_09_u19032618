77fb47a4e3854340a0b3f6f0aaf9079b5b038084
Sarah079 <u19032618@tuks.co.za>
2022-06-08 12:36:57 +0200

    Add project files.

---
+++ Prac_09_u19032618/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prac_09_u19032618.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult AddData()
+        {
+            return View();
+        }
+
+        public ActionResult ShowData()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Prac_09_u19032618/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prac_09_u19032618.MvcApplication" Language="C#" %>
